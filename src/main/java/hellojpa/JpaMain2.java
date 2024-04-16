c797d1a3d3365cd53fb4709c39aa2be4563d96f9
김승현 <dsagom@gmail.com>
2024-04-16 18:52:35 +0900

    section 4 : 엔티티 매핑 - 기본 키 매핑

---
+++ src/main/java/hellojpa/JpaMain2.java
@@ -0,0 +1,37 @@
+package hellojpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class JpaMain2 {
+
+    public static void main(String[] args) {
+
+        // 하나만 생성해서 Application 전체에서 공유
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        // 쓰레드간 공유 X ( 사용하고 버려야 한다.)
+        EntityManager em = emf.createEntityManager();
+
+        // JPA의 모든 데이터 변경은 Transaction 안에서 실행
+        EntityTransaction tx = em.getTransaction(); // DB Connection Get
+
+        tx.begin(); // // Transaction 시작
+
+        try {
+            Member member = new Member();
+            member.setUsername("C");
+
+            em.persist(member);
+
+            tx.commit(); // Commit
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close(); // Connection 반환
+            emf.close();
+        }
+    }
+}
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,25 +1,45 @@
 package hellojpa;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 public class Member {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, length = 10)
-    private String name;
+    @Column(name = "name", nullable = false) // 컬럼 매핑
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING) // enum 타입 매핑
+    private RoleType roleType;
+
+    // LocalDate LocalDateTime을 사용할 때는 생략 가능 (최신 하이버네이트 지원)
+    private LocalDate testLocalDate;
+    private LocalDateTime testLocalDateTime;
+
+    @Temporal(TemporalType.TIMESTAMP) // 날짜 매핑
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob // BLOB, CLOB 매핑
+    private String description;
+
+    // DB와 절대 관계 없는 field -> 메모리에서만 사용
+    /*@Transient
+    private int temp;*/
 
     protected Member() {
 
-    }
-
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
     }
 
     public Long getId() {
@@ -30,11 +50,67 @@ public class Member {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public LocalDate getTestLocalDate() {
+        return testLocalDate;
+    }
+
+    public void setTestLocalDate(LocalDate testLocalDate) {
+        this.testLocalDate = testLocalDate;
+    }
+
+    public LocalDateTime getTestLocalDateTime() {
+        return testLocalDateTime;
+    }
+
+    public void setTestLocalDateTime(LocalDateTime testLocalDateTime) {
+        this.testLocalDateTime = testLocalDateTime;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
